d8afa0bb576a580387ca2f0e4ab89e3a01d20031
Dennis Collon <dennis.collon@secucloud.com>
2019-08-19 16:50:33 +0200

    replaced placeholders

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'extension.createFolderStructure',
+    'FFS.createFolderStructure',
     createFolderStructure,
   );
 
