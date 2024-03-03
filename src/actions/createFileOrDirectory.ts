aa50fc8fcd902432c45f78dcc101e88779ca2912
Huuums <dennis.collon@gmail.com>
2024-03-03 15:52:57 +0100

    fix #125

--- src/actions/createFileOrDirectory.ts
+++ src/actions/createFileOrDirectory.ts
@@ -1,52 +1,39 @@
-import * as vscode from "vscode";
-import { FileSettings, TemplateNotation } from "../types";
+import * as vscode from 'vscode';
+import { FileSettings, TemplateNotation } from '../types';
 
-import { createDirectory, fileExists, getFileContent } from "../lib/fsHelpers";
-import { replaceAllVariablesInString } from "../lib/stringHelpers";
+import {
+  createDirectory,
+  writeToFile,
+  fileExists,
+  getFileContent,
+} from '../lib/fsHelpers';
+import { replaceAllVariablesInString } from '../lib/stringHelpers';
+import { dirname } from 'path';
 
-export default (
-    wsedit: vscode.WorkspaceEdit,
-    templateNotation: TemplateNotation
-  ) =>
+export default (templateNotation: TemplateNotation) =>
   async (file: FileSettings) => {
-    if (file.template === "EmptyDirectory") {
+    if (file.template === 'EmptyDirectory') {
       createDirectory(file.fileName);
       return null;
     }
 
-    const newPath = vscode.Uri.file(file.fileName);
     if (fileExists(file)) {
-      if (file.template?.includes("__existingcontent__")) {
+      if (file.template?.includes('__existingcontent__')) {
         const currentFileContent = getFileContent(file.fileName);
-        wsedit.replace(
-          newPath,
-          new vscode.Range(
-            new vscode.Position(0, 0),
-            new vscode.Position(99999, 99999)
-          ),
+        writeToFile(
+          file.fileName,
           replaceAllVariablesInString(
             file.template as string,
-            [["__existingcontent__", currentFileContent || ""]],
+            [['__existingcontent__', currentFileContent || '']],
             templateNotation
           )
         );
       } else {
-        wsedit.replace(
-          newPath,
-          new vscode.Range(
-            new vscode.Position(0, 0),
-            new vscode.Position(99999, 99999)
-          ),
-          file.template as string
-        );
+        writeToFile(file.fileName, file.template as string);
       }
     } else {
-      wsedit.createFile(newPath);
-      wsedit.insert(
-        newPath,
-        new vscode.Position(0, 0),
-        file.template as string
-      );
+      createDirectory(dirname(file.fileName));
+      writeToFile(file.fileName, file.template as string);
     }
 
     if (file.template) {
